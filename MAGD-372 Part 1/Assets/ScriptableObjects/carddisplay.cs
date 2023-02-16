d83cb17a2000be3391d89f6b5e03b98e82ee44f2
staddlern <staddlern@gmail.com>
2023-02-16 16:31:33 -0600

    Finished "Scriptable Objects" tutorial

--- MAGD-372 Part 1/Assets/ScriptableObjects/carddisplay.cs
+++ MAGD-372 Part 1/Assets/ScriptableObjects/carddisplay.cs
@@ -2,21 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class carddisplay : MonoBehaviour
 {
 
     public Card card;
 
-    public Text nameText;
-    public Text descriptionText;
-    public Text energyText;
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI effectText;
+    public TextMeshProUGUI energyText;
 
     public Image typeImage;
     public Image artImage;
 
     void Start()
     {
-        
+        nameText.text = card.name;
+        effectText.text = card.effect;
+
+        artImage.sprite = card.artwork;
+        typeImage.sprite = card.type;
+
+        energyText.text = card.energyCost.ToString();
     }
 }
